1c8bf63d7f4898619070a02c9cf6192a17eb2cf4
Nik0953 <7900953@gmail.com>
2022-08-03 18:21:08 +0400

    вDSM parsing ver2

--- txt_stat.py
+++ txt_stat.py
@@ -46,50 +46,6 @@ def mentioned_words(txt, words_in_rating):
         print(i, mentioned_words[i][0],mentioned_words[i][1])
 
     return mentioned_words[:words_in_rating]
-# ********************
 
-with open('articles.json', 'r') as f:
-    articles = json.load(f)
-
-print('количество новостей:', len(articles))
-
-
-# *******   создаем словарь    *******
-# собираем все тексты в один
-txt_all = ""
-for art in articles:
-    txt_all += art['text']
-
-print('Статистический анализ всех текстов:')
-mentioned_words(txt_all, 100)
-
-# количество последних новостей для сравнения с предыдущими
-last_news_number = 400
-top_wrds = 75
-
-print('\n\n', '*'*80)
-
-print('Анализ последних', last_news_number, 'текстов по сравнению с предыдущими:')
-txt_last = ""
-for art in articles[:last_news_number]:
-    txt_last += art['text']
-last_wrds = mentioned_words(txt_last, top_wrds)
-
-print('\n\n', '*'*80)
-print('Предыдущие тексты:')
-txt_prev = ""
-for art in articles[last_news_number:]:
-    txt_prev += art['text']
-prev_wrds = mentioned_words(txt_prev, top_wrds)
-
-#print(last_wrds)
-
-# Новые слова в рейтинге:
-last_wrds_lst = [wrd[0] for wrd in last_wrds]
-prev_wrds_lst = [wrd[0] for wrd in prev_wrds]
-print('\n\n\n ******    Новые слова в рейтинге:    ******')
-for wrd in last_wrds_lst:
-    if wrd not in prev_wrds_lst:
-        print(wrd)
 
 
